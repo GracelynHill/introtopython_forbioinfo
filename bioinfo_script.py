65033871283f68de4a7cf435d276a28644550cc0
Gracelyn Hill <72661952+GracelynHill@users.noreply.github.com>
2022-11-28 13:23:18 -0500

    Update bioinfo_script.py

--- bioinfo_script.py
+++ bioinfo_script.py
@@ -1,66 +1,66 @@
-#!/usr/bin/python3
-#This script is adapted from Francois Chollet's book "Deep Learning in Python"
-#My goal with this is just to get practice for ML classification problems
+# This program takes in the name of a FASTA formatted file as an argument, and returns a new file called "nucleotide_counts.tsv" that contains the frequency of each nucelotide in each sequence, the frequency of nucleotides across all sequences, and the ratio of transitions to transversions (comparing the first sequence to all other sequences.) To run the program, write something like this in the command line program of your choice:
+# \. Lab4.py example_file.fasta
 
-import tensorflow
-from keras.datasets import imdb
-import numpy as np
-#dataset is a list of imdb reviews, labeled as positive or negative. our goal is review sentiment prediction :)
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
-#train_data and test_data are review lists, train_labels and test_labels are their sentiment
-#we limit to the 10000 most frequent words in order to make the data more managable
-#each record in the data is a a list of words by their popularity index
+import sys
+#u_input = str(sys.argv[0]) # This line takes in the input from the command line
+input_file = open('primatesNuc.fasta', 'r') # This opens the input as a file
+output_file = open('nucleotide_counts.tsv', 'w') # creating the nucleotide_counts file
+output_file.write('Gene\tA\tC\tG\tT\n') #writing the header
+sequences = [] #An empty list of sequences that will be appended later in the script
+transition = 0 #blank variables for transitions and transversions that will be edited by a loop later in the script
+transversion = 0
+AG = ['A', 'G'] #lists defining purines and pyrimidines
+CT = ['C', 'T']
+counter = 1 # a counter variable that will be used to loop through the list of sequences, caluclating Transition/transversion ratio. It starts at 1 so that it does not count the first sequence.
+from Bio.Seq import Seq #Importing biopython modules that make the process of counting nucleotides and reading sequences easier
+from Bio import SeqIO
+for cur_record in SeqIO.parse(input_file, "fasta"): # This uses biopython to read the file in fasta format. Biopython understands which sections of the file are headers, names, sequences, etc.
+    gene_name = cur_record.name
+    length = len(cur_record.seq)
+    A_count = cur_record.seq.count('A') # Counting the number of all four nucleotides
+    C_count = cur_record.seq.count('C')
+    G_count = cur_record.seq.count('G')
+    T_count = cur_record.seq.count('T')
+    A_freq = float((A_count / length) * 100) # changing the nucleotide counts into percentages
+    C_freq = float((C_count / length) * 100)
+    G_freq = float((G_count / length) * 100)
+    T_freq = float((T_count / length) * 100)
+    sequences.append(str(cur_record.seq)) #adding all of the sequences to a list that will be used later
+    output_line = '%s\t%i\t%i\t%i\t%i\n' % \
+    (gene_name, A_freq, C_freq, G_freq, T_freq) #printing out the percentages
+    output_file.write(output_line)
 
-def vectorize_sequences(sequences, dimension=10000):
-	results = np.zeros(len(sequences), dimension)
-	for i, sequence in enumerate(sequences):
-		results[i, sequence] = 1.
-	return results
-#a list of lists isn't a tensor, so we need to make it one
-#vectorization is our solution: a list [3, 5] becomes a 10,000 dimensional vector of all zeros except for 1s at 3 and 5
-
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
-#vectorized data
-
-y_train = np.asarray(train_labels).astype('float32')
-y_train = np.asarray(test_labels).astype('float32') 
-#labels are already 1 or 0, so vectorization is trivial
-
-from keras import models
-from keras import layers
-
-model = models.Sequential()
-model.add(layers.Dense(16, activation = 'relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation = 'relu'))
-model.add(layers.Dense(1, activation = 'sigmoid'))
-#two layers for the tensor operation relu(dot(W, input) + b), allowing a hypothesis space for learning
-#final layers collapses it into a sigmoid function for sentiment probability
-
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-#adds our optimize & loss function
-
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
-#sets a side a portial of data for validation
-
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
-
-#The model part is finished! The rest is just using matplotlib for success statistics ^_^
-
-import matplotlib.pyplot as plt
-history_dict = history.history
-loss_values = history_dict['loss']
-val_loss_values = history_dict['val_loss']
-epochs = range(1, len('acc') + 1)
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label= 'Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('TrainValLoss.png')
-
+fullfrequencyseq = Seq('') #this section adds all of the sequences together into a single sequence
+for n in sequences:
+  fullfrequencyseq += n
+flength = len(fullfrequencyseq) #using the same logic as the last loop to calculate frequency across all sequences
+A_fcount = fullfrequencyseq.count('A')
+C_fcount = fullfrequencyseq.count('C')
+G_fcount = fullfrequencyseq.count('G')
+T_fcount = fullfrequencyseq.count('T')
+A_ffreq = float((A_fcount / flength) * 100)
+C_ffreq = float((C_fcount / flength) * 100)
+G_ffreq = float((G_fcount / flength) * 100)
+T_ffreq = float((T_fcount / flength) * 100)
+output_file.write("frequency across all sequences: " + '\n' + str(A_ffreq) + '\t' + str(C_ffreq) + '\t' + str(G_ffreq) + '\t' + str(T_ffreq) + '\n') #writing the results
+output_file.write("Number to the left is the sequence line number. Number to the right is the transition/transversion ratio of that sequence to the first sequence" + '\n') #explaining the formatting of the next section
+for n in sequences:
+    while counter < len(sequences):
+        s1 = sequences[0] #defining the 2 sequences in the list that will be operated on for the transition/transversion ratio
+        s2 = sequences[counter]
+        counter = counter + 1
+        for nt1, nt2 in zip(s1, s2): #looping through the sequences to find transitions and transversions, counting up if they are found
+            if nt1 != nt2:
+                if nt1 in AG and nt2 in AG:
+                    transition += 1
+                elif nt1 in CT and nt2 in CT:
+                    transition += 1
+                else:
+                    transversion += 1
+        titv = (' ' '%0.11f' % (transition / transversion)) #calculates the ratio
+        output_file.write(str(counter + 1) + titv + '\n') #writes the ratio to the file
+        transition = 0 #resetting the number of transitions and transversions
+        transversion = 0
+input_file.close() #closing the file
+output_file.close()
